40d753dda32a8110904c5027840e006ad541a3b0
Ahsan Joiya <iamAhsanJoiya@outlook.com>
2022-06-09 09:33:42 +0500

    Initial Commit

---
+++ API/Entities/TblEmployee.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities;
+
+public class TblEmployee
+{
+  [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+  public Guid Id { get; set; }
+  [StringLength(30)]
+  public string? FirstName { get; set; }
+  [StringLength(30)]
+  public string? LastName { get; set; }
+  [StringLength(250)]
+  public string? Email { get; set; }
+  public DateTime? Dob { get; set; }
+  public string? Gender { get; set; }
+  public int IsMarried { get; set; }
+  public int IsActive { get; set; }
+  [ForeignKey("DesignationId")]
+  public int DesignationId { get; set; }
+  [NotMapped]
+  public string? Designation { get; set; }
+
+}
